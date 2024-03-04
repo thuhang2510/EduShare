ecb01b1f3b05d88835714d8ace76535870f6848c
thuhang2510 <you@example.com>
2024-03-04 09:06:16 +0700

    test send mail from momo

---
+++ app/momo/email.py
@@ -0,0 +1,13 @@
+from flask import render_template
+from app.email import send_email
+
+def send_email_field(user):
+    try:
+        send_email(('[EduShare] Nạp tiền vào tài khoản'),
+                recipients=[user.email],
+                html_body=render_template('email/update_field.html',
+                                            user=user))
+        
+        return None, 0, "Send email success"
+    except Exception as e:
+        return None, -1, "Send email fail " + str(e) 
--- app/momo/routes.py
+++ app/momo/routes.py
@@ -1,7 +1,12 @@
 from flask_login import current_user
 from app.auth import bp
+from app.model.models import User
 from app.momo.email import send_email_field
 
 @bp.route("/send-mail", methods=["GET"])
 def sendMail():
-    send_email_field(current_user)
+    user = User()
+    user.email = "thu"
+    user.number = "131313"
+    user.set_password("hang")
+    user.save_to_db()
