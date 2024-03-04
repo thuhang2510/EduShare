458c286780f6dc125faf5e012c312db45180d08c
thuhang2510 <you@example.com>
2024-03-04 08:45:48 +0700

    test send mail from momo

--- app/momo/routes.py
+++ app/momo/routes.py
@@ -2,6 +2,6 @@ from flask_login import current_user
 from app.auth import bp
 from app.momo.email import send_email_field
 
-@bp.route("/send-mail", methods=["POST"])
+@bp.route("/send-mail", methods=["GET"])
 def sendMail():
     send_email_field(current_user)
