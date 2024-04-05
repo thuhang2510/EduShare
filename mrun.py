fb34296e4ed4d9c037bc063999e6cdd690678014
thuhang2510 <you@example.com>
2024-04-05 19:58:20 +0700

    feat: update code

--- mrun.py
+++ mrun.py
@@ -1,17 +1,6 @@
 from app import create_app
-import os
-import aspose.words as aw
-
-lic = aw.License()
 
 app = create_app()
 
 if __name__ == "__main__":
-    try:
-        lic.set_license("Aspose.WordsforPythonvia.NET.lic")
-        print("License set successfully.")
-        
-    except RuntimeError as err:
-        print("Không thể license")
-    
     app.run(debug=True)
--- ultil/file_helper.py
+++ ultil/file_helper.py
@@ -6,12 +6,10 @@ lic = aw.License()
 def convert_doc_to_pdf(file_path_old, file_path_new):
     try:
         lic.set_license("Aspose.WordsforPythonvia.NET.lic")
-        return "", 0, None
         print("License set successfully.")
         
         doc = aw.Document(file_path_old)
         doc.save(file_path_new)
         os.remove(file_path_old)
-        return "", 0, None
     except RuntimeError as err:
-        return "Không thể license", -1, None
+        print("\nThere was an error setting the license:", err)
