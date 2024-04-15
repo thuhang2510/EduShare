9a6d86c39cee2dbfb3a5af453ef23e56239286cf
thuhang2510 <you@example.com>
2024-04-16 00:29:58 +0700

    feat: user background task for loading doc to ai

--- app/ai/tasks.py
+++ app/ai/tasks.py
@@ -4,12 +4,17 @@ from app.document.services import DocumentsDataService
 
 @shared_task(ignore_result=False)
 def page_pdf_and_build_vector_db(url, document_edu_name, document_id, user_id, api_key=None):
+    print("vo")
     _, code, _ = AIDataService().page_pdf_and_build_vector_db(url, document_edu_name, document_id, user_id, api_key)
+
+    print(code)
     
     status = 0
     if code == 0:
         status = 1
     else:
         status = -1
+
+    print("update status")
         
     DocumentsDataService().update_processing_status(status, document_id)
